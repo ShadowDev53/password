34a65d5fc8c2ef46c87abd4a2e0b807b391efddd
ShadowDev53 <khai.murad28@student.tamdistrict.org>
2025-11-20 13:52:37 -0800

    created lists and debugged input

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-const hi = document.getElementById("test")
+const hi = document.getElementsByName("test")
 let check = 0
-check.addEventListener("change", function (event) {
-    for (let i in hi) {
+const capLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+const lowLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+const symbols = ["`", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "+", "=", "[", "]", "{", "}", "|", ";", ":", "'", ",", "<", ".", ">", "/", "?"]
+const passwordList = []
+
+for (const i of hi) {
+    i.addEventListener("change", function () {
+
         if (this.checked) {
-            this.checked = false
+            passwordList.push(capLetters)
             
+            console.log(passwordList)
+
+        } else {
+            console.log("no")
         }
         console.log(this.checked)
-    }
-})
+
+
+    })
+
+}
